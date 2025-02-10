1e177d5ba2d7e03c5f6d9477f2b04384afda484b
David Cruz <dcruzer92@gmail.com>
2025-02-10 07:11:46 -0600

    Time restriction Middleware has been updated

--- middleware/timeRestrictionMiddleware.js
+++ middleware/timeRestrictionMiddleware.js
@@ -2,8 +2,8 @@ const moment = require("moment");
 
 const timeRestrictionMiddleware = (request, response, next) => {
   const currentTime = moment();
-  const startTime = moment().set({ hour: 0, minute: 30, second: 0 });
-  const endTime = moment().set({ hour: 23, minute: 30, second: 0 });
+  const startTime = moment().set({ hour: 8, minute: 30, second: 0 });
+  const endTime = moment().set({ hour: 16, minute: 30, second: 0 });
 
   if (currentTime.isBefore(startTime) || currentTime.isAfter(endTime)) {
     return response.status(403).json({
