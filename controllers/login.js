767a9905d0f14f2913ca7a3def6fcbe9452a8d3b
David Cruz <dcruzer92@gmail.com>
2025-02-09 22:21:59 -0600

    Refactorized clientIP function

--- controllers/login.js
+++ controllers/login.js
@@ -2,7 +2,6 @@ const db = require("../config/db");
 const crypto = require("crypto");
 const jwt = require("jsonwebtoken");
 const audit = require("../global/audit/audit");
-const clientIp = require("get-client-ip");
 
 exports.login = (request, response) => {
   const { username, password } = request.body;
@@ -31,12 +30,7 @@ exports.login = (request, response) => {
         .json({ message: "Usuario o Contraseña no Válidos" });
     }
 
-    audit(
-      result[0].id,
-      "Inicio de Sesión",
-      "Inicio de Sesión Correcto",
-      clientIp(request)
-    );
+    audit(result[0].id, "Inicio de Sesión", "Inicio de Sesión Correcto");
 
     const token = jwt.sign(
       { id: result[0].id, username: result[0].username },
--- global/audit/audit.js
+++ global/audit/audit.js
@@ -1,12 +1,13 @@
 const db = require("../../config/db");
+const clientIP = require("get-client-ip");
 
-function audit(user_id, action, details, client_details) {
+function audit(user_id, action, details) {
   const audit =
     "INSERT INTO audit (user_id, action, details, client_details, date_hour) VALUES (?, ?, ?, ?, ?)";
 
   db.query(
     audit,
-    [user_id, action, details, client_details, new Date()],
+    [user_id, action, details, clientIP(request), new Date()],
     (error, result) => {
       if (error) {
         console.error(error);
