3430c0b034901c031ce7259a574ac1d57423f117
David Cruz <dcruzer92@gmail.com>
2025-02-10 00:48:41 -0600

    Audit data

--- global/audit/audit.js
+++ global/audit/audit.js
@@ -5,7 +5,7 @@ const useragent = require("useragent");
 function audit(user_id, action, details, request) {
   const clientIp = getClientIP(request);
   const agent = useragent.parse(request.headers["user-agent"]);
-  const clientDetails = `${agent.toAgent()}; ${agent.os.toString()}; ${clientIp}`;
+  const clientDetails = `Desde: ${agent.toAgent()}; OS: ${agent.os.toString()}; IP: ${clientIp}`;
 
   const audit =
     "INSERT INTO audit (user_id, action, details, client_details, date_hour) VALUES (?, ?, ?, ?, ?)";
